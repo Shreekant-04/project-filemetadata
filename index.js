6b47832760e04c7b641502437843d68f408b1165
Shreekant <Shreekant4062@gmail.com>
2024-11-12 14:43:15 +0530

    "Updated index.js to use EJS template engine, added EJS dependency, removed index.html file and updated package.json accordingly."

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var cors = require("cors");
+var path = require("path");
 require("dotenv").config();
 
 var app = express();
@@ -7,10 +8,12 @@ var app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/public", express.static(process.cwd() + "/public"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
+  res.render("index");
 });
 
 const multer = require('multer');
@@ -20,7 +23,6 @@ app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
    if (!req.file) {
        return res.status(400).send({ error: "No file uploaded" });
    }
-   console.log(req.file, req.body);
    res.send({
        filename: req.file.originalname,
        size: req.file.size,
